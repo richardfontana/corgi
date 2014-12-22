912743e335abd77836a792ce598e948ba83f477f
Richard Fontana <fontana@sharpeleven.org>
2014-12-21 17:18:11 -0800

    refine alias lookup and handling of attachments

--- corgi/__init__.py
+++ corgi/__init__.py
@@ -1,16 +1,21 @@
-#!/usr/bin/env python
-
 
 import argparse
+import email
+
+from collections import namedtuple
+from email import message
+
+import idna
 
 
 corgi_cfg = "config.yml"
 
-alias_dict={}
+alias_dict = {}
 
 
 def get_arg_parser():
-    parser = argparse.ArgumentParser(prog='corgi')
+
+    parser = argparse.ArgumentParser(prog = 'corgi')
 
     parser.add_argument('-A', metavar='alias', nargs='+',
                         dest='alias_queries', 
@@ -25,17 +30,24 @@ def get_arg_parser():
 
 def lookup_alias(k):
     if k in alias_dict:
-        return alias_dict[k]
+        return idna.encode(alias_dict[k])
     else:
         return k
 
 
 def main():
     args = get_arg_parser().parse_args()
-    alias_result = [lookup_alias(k) for k in args.alias_queries or []]
-    for i in alias_result:
-        print i
+    aq = args.alias_queries
+    if aq:
+        alias_result = [lookup_alias(k) for k in aq]
+        for r in alias_result:
+            print r
+        return
+    else:
+        msg = message.Message()
+        for a in args.attach_list:
+            msg.attach(a)
+    
       
-
 if __name__ == '__main__':
     main()
